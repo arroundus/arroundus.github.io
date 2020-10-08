e6c2401b54953185cda2d249578384e7c2cb7772
arroundus <dimkahogy@gmail.com>
2020-10-08 16:52:11 +0300

    +

---
+++ js/сheckngo.js
@@ -0,0 +1,22 @@
+let name = document.querySelector('#name')
+let mail = document.querySelector('#email')
+let phone = document.querySelector('#phone')
+let message = document.querySelector('#message')
+let button = document.querySelector('button')
+let alertsuccess = document.querySelector('.alert-success')
+let alertdanger = document.querySelector('.alert-danger')
+
+
+
+button.onclick = function() {
+    
+    if (((phone.value).length) < 16 || ((name.value).length <= 1) || ((mail.value).length < 5)) {
+        alert('Проверьте правильность ввода');
+        return;
+        message.value = name.value + mail.value + phone.value + 'Не работает';
+    }
+    else if (((phone.value).length) == 16 || ((name.value).length > 1) || ((mail.value).length > 5)) {
+        alert('Сообщение отправлено');
+        message.value = name.value + mail.value + phone.value + 'Работает'
+    }
+}
